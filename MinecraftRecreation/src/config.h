d8605fe9a1f84e1b10f6c89f1c7e38b709dd615d
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2024-06-29 02:02:38 +0100

    prepared config.h

--- MinecraftRecreation/src/config.h
+++ MinecraftRecreation/src/config.h
@@ -1,6 +1,14 @@
 #pragma once
 
 struct config {
+	// Window Settings
 	const static int resolutionX = 1920;
 	const static int resolutionY = 1080;
+
+	// Camera Settings
+	constexpr static float sensitivity = 0.1f;
+	constexpr static float speed = 2.5f;
+	const static int zoom = 45;
+	const static int yaw = -90;
+	const static int pitch = 0;
 };
