861efdecb30c24421cfd385355dfa86387d756a0
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2024-06-29 17:04:15 +0100

    moved camera

--- MinecraftRecreation/src/config.h
+++ MinecraftRecreation/src/config.h
@@ -8,9 +8,9 @@ struct config {
 	const static int resolutionY = 1080;
 
 	// Camera Settings
-	constexpr static glm::vec3 cameraStartPosition = glm::vec3(-1.0f, 1.0f, 0.0f);
+	constexpr static glm::vec3 cameraStartPosition = glm::vec3(0.0f, 2.0f, 0.0f);
 	constexpr static float cameraSensitivity = 0.1f;
-	constexpr static float cameraSpeed = 0.01f;
+	constexpr static float cameraSpeed = 0.05f;
 	const static int zoom = 85;
 	const static int yaw = -90;
 	const static int pitch = 0;
