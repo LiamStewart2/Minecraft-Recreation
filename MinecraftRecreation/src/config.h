217ffe98f5128d2e29bf16d4c9764b7c9e8b585b
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2024-07-01 21:04:28 +0100

    increased speed of camera

--- MinecraftRecreation/src/config.h
+++ MinecraftRecreation/src/config.h
@@ -10,7 +10,7 @@ struct config {
 	// Camera Settings
 	constexpr static glm::vec3 cameraStartPosition = glm::vec3(0.0f, 22.0f, 0.0f);
 	constexpr static float cameraSensitivity = 0.1f;
-	constexpr static float cameraSpeed = 0.05f;
+	constexpr static float cameraSpeed = 0.5f;
 	const static int zoom = 85;
 	const static int yaw = -90;
 	const static int pitch = 0;
